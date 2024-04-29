a6074b1e4335b37aa5a9717e4719f6ce48418e3f
Arthur Hauser <arthur.hauser@gmail.com>
2024-04-28 20:01:24 -0400

    add css styling

---
+++ assets/legal memoranda/curatedDocs.js
@@ -0,0 +1,30 @@
+// curatedDocs.js
+export const curatedDocs = [
+
+    {
+        id: 'default-notary-doc-a',
+        documentName: 'Acknowledgement of Conveyance Form',
+        path: 'assets/legal memoranda/Acknowledgement of Conveyance Form.txt',
+        documentSize: '762', // Replace 'placeholder' with the actual file size
+        documentType: 'txt', // Replace 'placeholder' with the actual file type
+        lastModified: 'Friday, March 15, 2024, 1:30:15 PM' // Replace 'placeholder' with the actual last modified date
+    },
+    {
+        id: 'default-notary-doc-b',
+        documentName: 'Certificate of Subscribing Witness Form',
+        path: 'assets/legal memoranda/Certificate of Subscribing Witness Form.txt',
+        documentSize: '905', // Replace 'placeholder' with the actual file size
+        documentType: 'txt', // Replace 'placeholder' with the actual file type
+        lastModified: 'Friday, March 15, 2024, 1:31:01 PM' // Replace 'placeholder' with the actual last modified date
+    },
+    {
+        id: 'default-notary-doc-c',
+        documentName: 'Corporate Acknowledgement Form',
+        path: 'assets/legal memoranda/Corporate Acknowledgement Form.txt',
+        documentSize: '1800', // Replace 'placeholder' with the actual file size
+        documentType: 'txt', // Replace 'placeholder' with the actual file type
+        lastModified: 'Friday, March 15, 2024, 1:29:35 PM' // Replace 'placeholder' with the actual last modified date
+    },
+];
+
+module.exports = { curatedDocs };
